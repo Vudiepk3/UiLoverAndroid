f6278b911a5c9df319c395d7d7c3cfeb0ef73400
Vudiepk3 <110141392+Vudiepk3@users.noreply.github.com>
2024-07-23 09:19:20 +0700

    add  Demo_CustomListView/

---
+++ Demo_CustomListView/app/src/main/java/com/example/demo_customlistview/ListData.java
@@ -0,0 +1,14 @@
+package com.example.demo_customlistview;
+
+public class ListData {
+    String name, time;
+    int ingredients, desc;
+    int image;
+    public ListData(String name, String time, int ingredients, int desc, int image) {
+        this.name = name;
+        this.time = time;
+        this.ingredients = ingredients;
+        this.desc = desc;
+        this.image = image;
+    }
+}
